8f4d1c053f3f649014665aece1be6b1d16a7e2a8
kelmorgan <kelmorgan18@gmail.com>
2022-08-10 10:42:09 +0100

    Update to datastructure

---
+++ src/main/java/com/kelmorgan/datastructure/BigO.java
@@ -0,0 +1,9 @@
+package com.kelmorgan.datastructure;
+
+public class BigO {
+
+    public void  log(int [] numbers){
+        for (int number : numbers)
+            System.out.println(number);
+    }
+}
--- src/main/java/com/kelmorgan/datastructure/Main.java
+++ src/main/java/com/kelmorgan/datastructure/Main.java
@@ -3,5 +3,11 @@ package com.kelmorgan.datastructure;
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println( Runtime.getRuntime().totalMemory());
+        System.out.println( Runtime.getRuntime().maxMemory());
+        System.out.println(  Runtime.getRuntime().freeMemory());
+
+
     }
 }
