7bf5cba0417d606c1a1c8a41a5e46929977358ee
kelmorgan <kelmorgan18@gmail.com>
2022-08-21 19:03:10 +0100

    update

--- src/main/java/com/kelmorgan/datastructure/BigO.java
+++
@@ -1,9 +0,0 @@
-package com.kelmorgan.datastructure;
-
-public class BigO {
-
-    public void  log(int [] numbers){
-        for (int number : numbers)
-            System.out.println(number);
-    }
-}
--- src/main/java/com/kelmorgan/datastructure/Main.java
+++ src/main/java/com/kelmorgan/datastructure/Main.java
@@ -1,13 +1,22 @@
 package com.kelmorgan.datastructure;
 
+import java.util.Arrays;
+
 public class Main {
+
+    private static long[] fibonacciCache;
+
     public static void main(String[] args) {
 
+        var list = new LinkedList();
 
-        System.out.println( Runtime.getRuntime().totalMemory());
-        System.out.println( Runtime.getRuntime().maxMemory());
-        System.out.println(  Runtime.getRuntime().freeMemory());
-
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addFirst(5);
+        System.out.println(list.getKthFromTheEnd(3));
 
     }
+
 }
